b59bddaa35a45939721a1f462b2a7fb1e621d965
Gadai-iiyokoiyo <121931749+Gadai-iiyokoiyo@users.noreply.github.com>
2023-03-23 00:41:27 +0900

    Add files via upload

--- pkgsetup.sh
+++
@@ -1,4 +0,0 @@
-wget "https://github.com/Gadai-iiyokoiyo/BactPkg/blob/BactLua/hello?raw=true" -O "BactLua/Lua"
-echo "--------------------------------------------------"
-echo "BactLua Installed"
-echo "--------------------------------------------------"
