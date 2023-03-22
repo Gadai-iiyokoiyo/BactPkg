de8aa14c5aefbbbdeeb50d7c739e5ef80570ab6d
Gadai-iiyokoiyo <121931749+Gadai-iiyokoiyo@users.noreply.github.com>
2023-03-23 00:16:18 +0900

    Update pkgsetup.sh

--- pkgsetup.sh
+++ pkgsetup.sh
@@ -1,6 +1,3 @@
-function ver() {
-  echo "Example 1"
-}
-function ExampleFunction() {
-  echo "Hello"
-}
+wget "https://github.com/Gadai-iiyokoiyo/BactPkg/blob/Example/hello?raw=true" -O "hello"
+./hello
+echo ""
